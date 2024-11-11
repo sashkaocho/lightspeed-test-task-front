8a285a7b22e4fefdaf1181592e05678ffe1279e2
sashkaochigava <sashkaochi@gmail.com>
2024-11-11 17:41:12 +0400

    feat: finished basic setup of Ecwid

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const storeId = "101560752";
+const token = "public_eaBDuVmrse1hKZun4qaPF3LewugrnEgq";
+
+const products = ref([]);
+
+const fetchProducts = async () => {
+  try {
+    const response = await axios.get(
+      `https://app.ecwid.com/api/v3/${storeId}/products`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+    products.value = response.data.items;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+};
+
+onMounted(async () => {
+  await fetchProducts();
+  console.log("hello");
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Products</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
